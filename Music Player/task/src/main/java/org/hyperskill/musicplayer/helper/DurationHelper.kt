858d2e057c5e4d1f10cb218eaf3998fc89a3b727
nero <hungnd.vnse@gmail.com>
2024-01-04 17:49:41 +0700

    [fix(formatMilliseconds)]

--- Music Player/task/src/main/java/org/hyperskill/musicplayer/helper/DurationHelper.kt
+++ Music Player/task/src/main/java/org/hyperskill/musicplayer/helper/DurationHelper.kt
@@ -7,5 +7,5 @@ fun formatMilliseconds(milliseconds: Int): String {
     val duration = milliseconds.toDuration(DurationUnit.MILLISECONDS)
     val minutes = duration.inWholeMinutes
     val seconds = duration.minus(minutes.toDuration(DurationUnit.MINUTES)).inWholeSeconds
-    return "${"%02d".format(minutes)}:$seconds"
+    return "%02d:%02d".format(minutes, seconds)
 }
